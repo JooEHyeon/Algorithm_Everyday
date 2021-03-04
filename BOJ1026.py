59ae28c41cf4a1031949653d190995b8a8f1128b
pjh98 <pjh980918@gmail.com>
2021-03-05 00:18:42 +0900

    [그리디] 보물

---
+++ BOJ1026.py
@@ -0,0 +1,19 @@
+n = int(input())
+a = []
+b = []
+
+a = list(map(int,input().split()))
+b = list(map(int,input().split()))
+
+a.sort()
+b.sort()
+
+s = 0
+for i in range(n):
+    s+=(a[i]*b[n-i-1])
+
+print(s)
+
+
+
+
